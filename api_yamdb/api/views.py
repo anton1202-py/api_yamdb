ffa104bc51378ca35306d466d3ebccc7839c3a5a
Кузнецов Антон <anton1202@yandex.ru>
2022-09-09 19:59:23 +0300

    Модели Review и Comment. Вьюшки к ним и serializer

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from reviews.models import Review
+from .permissions import IsAuthorStaffOrReadOnly
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorStaffOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorStaffOrReadOnly,)
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Category, Comment, Genre, Review, Title, User
+
+admin.site.register(Title)
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Review)
+admin.site.register(Comment)
