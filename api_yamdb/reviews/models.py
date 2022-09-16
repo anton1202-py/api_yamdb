8fa641804b603f7b7c6e4a39f1cb0f58dac3fb51
Пригарин Вадим <tralalacav@mail.ru>
2022-09-17 01:19:36 +0300

    Mixin в views, превышение года в модели с самописным валидатором.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,7 +8,7 @@ from users.models import User
 
 def validate_max_year(year_user):
     year = dt.datetime.today().year
-    if year_user >= year:
+    if year_user > year:
         raise ValidationError(
             'Год превышает нынешний'
         )
